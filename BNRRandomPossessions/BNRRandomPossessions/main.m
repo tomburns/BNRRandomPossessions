689768f8b44c0dbbe817dde1dd93005e0aac3bd7
Tom Burns <tomburns@gmail.com>
2013-06-22 18:38:08 -0400

    Initial Xcode Commit

---
+++ BNRRandomPossessions/BNRRandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BNRRandomPossessions
+//
+//  Created by Tom Burns on 6/22/13.
+//  Copyright (c) 2013 Tom Burns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
